31ac416a9fd4b65141179031ecde645fc8e3d9bb
rogelio.sevilla1 <rogelio.sevilla1@gmail.com>
2013-03-28 18:26:52 -0600

    rewrite user in jira's session when another saml response comes into jira

--- onelogin_jira_saml/src/main/java/com/onelogin/jira/saml/SSOAuthenticator.java
+++ onelogin_jira_saml/src/main/java/com/onelogin/jira/saml/SSOAuthenticator.java
@@ -6,11 +6,16 @@ import com.atlassian.seraph.auth.AuthenticatorException;
 import com.atlassian.seraph.auth.DefaultAuthenticator;
 import com.opensymphony.user.EntityNotFoundException;
 import com.opensymphony.user.UserManager;
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.security.Principal;
+import java.security.cert.CertificateException;
+import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
 import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
 
 public class SSOAuthenticator extends DefaultAuthenticator {
 
@@ -24,34 +29,17 @@ public class SSOAuthenticator extends DefaultAuthenticator {
     public Principal getUser(HttpServletRequest request, HttpServletResponse response) {
 
         Principal user = null;
-        XmlUtil xm = null;
-        String certificateS = "";
-        String setAssertionConsumerServiceUrl = "";
-        String setIssuer = "";
-        String setIdpSsoTargetUrl = "";
-        String sFile = "jira_onelogin.xml";
+        HashMap<String,String> configValues = getConfigurationValues("jira_onelogin.xml");
         String sSAMLResponse = request.getParameter("SAMLResponse");
 
         try {
-            if (request.getSession() != null && request.getSession().getAttribute(DefaultAuthenticator.LOGGED_IN_KEY) != null) {
-                log.info("Session found; user already logged in");
-                user = (Principal) request.getSession().getAttribute(DefaultAuthenticator.LOGGED_IN_KEY);
-            } else {
-                xm = new XmlUtil();
-                String sConf = xm.getConfigs(sFile);
-                String[] sConfTemp = sConf.split("\\|");
-                certificateS = sConfTemp[0];
-                setAssertionConsumerServiceUrl = sConfTemp[1];
-                setIssuer = sConfTemp[2];
-                setIdpSsoTargetUrl = sConfTemp[3];
-
                 if (sSAMLResponse != null) {
-                    // User account specific settings. Import the certificate here
-                    AccountSettings accountSettings = new AccountSettings();
-                    accountSettings.setCertificate(certificateS);
 
-                    Response samlResponse = new Response(accountSettings);
-                    samlResponse.loadXmlFromBase64(request.getParameter("SAMLResponse"));
+                    request.getSession().setAttribute(DefaultAuthenticator.LOGGED_IN_KEY,  null);
+                    request.getSession().setAttribute(DefaultAuthenticator.LOGGED_OUT_KEY, null);
+                    
+                    // User account specific settings. Import the certificate here
+                    Response samlResponse = getSamlResponse(configValues.get("certificate"),request.getParameter("SAMLResponse"));                    
 
                     if (samlResponse.isValid()) {
                         // The signature of the SAML Response is valid. The source is trusted
@@ -77,21 +65,27 @@ public class SSOAuthenticator extends DefaultAuthenticator {
                     } else {
                         log.error("SAML Response is not valid");
                     }
-                } else {
+                } 
+                else if (request.getSession() != null && request.getSession().getAttribute(DefaultAuthenticator.LOGGED_IN_KEY) != null) {
+                    log.info("Session found; user already logged in");
+                    user = (Principal) request.getSession().getAttribute(DefaultAuthenticator.LOGGED_IN_KEY);
+                 }
+                
+                else {
                     // The appSettings object contain application specific settings used by the SAML library
                     AppSettings appSettings = new AppSettings();
 
                     // Set the URL of the consume.jsp (or similar) file for this application. The SAML Response will be posted to this URL
-                    appSettings.setAssertionConsumerServiceUrl(setAssertionConsumerServiceUrl);
+                    appSettings.setAssertionConsumerServiceUrl(configValues.get("assertionConsumerServiceUrl"));
 
                     // Set the issuer of the authentication request. This would usually be the URL of the issuing web application
-                    appSettings.setIssuer(setIssuer);
+                    appSettings.setIssuer(configValues.get("issuer"));
 
                     // The accSettings object contains settings specific to the users account. At this point, your application must have identified the users origin
                     AccountSettings accSettings = new AccountSettings();
 
                     // The URL at the Identity Provider where the authentication request should be sent
-                    accSettings.setIdpSsoTargetUrl(setIdpSsoTargetUrl);
+                    accSettings.setIdpSsoTargetUrl(configValues.get("idpSsoTargetUrl"));
 
                     // Generate an AuthRequest and send it to the identity provider
                     AuthRequest authReq = new AuthRequest(appSettings, accSettings);
@@ -104,7 +98,7 @@ public class SSOAuthenticator extends DefaultAuthenticator {
 
                     request.getSession().setAttribute("reqString", reqString);
                 }
-            }
+            
         } catch (Exception e) {
             log.error("error while trying to send the saml auth request:" + e);
         }
@@ -112,6 +106,29 @@ public class SSOAuthenticator extends DefaultAuthenticator {
         return user;
     }
 
+    private Response getSamlResponse(String certificate,String responseEncrypted) throws CertificateException, ParserConfigurationException, SAXException, IOException {
+        AccountSettings accountSettings = new AccountSettings();
+        accountSettings.setCertificate(certificate);
+
+        Response samlResponse = new Response(accountSettings);
+        samlResponse.loadXmlFromBase64(responseEncrypted);
+        return samlResponse;
+    }
+
+    private HashMap<String, String> getConfigurationValues(String file) {
+        XmlUtil xm = new XmlUtil();
+        HashMap<String,String> configValues = new HashMap<String,String>();
+        String sConf = xm.getConfigs(file);
+        String[] sConfTemp = sConf.split("\\|");
+        configValues.put("certificate", sConfTemp[0]);
+        configValues.put("assertionConsumerServiceUrl", sConfTemp[1]);
+        configValues.put("issuer", sConfTemp[2]);
+        configValues.put("idpSsoTargetUrl", sConfTemp[3]);
+        return configValues;
+    }
+    
+    
+    
     @Override
     protected Principal getUser(String username) {
         /*
